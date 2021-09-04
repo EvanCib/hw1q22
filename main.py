3a3d493a2a5ee0b9d086726e7706fcc977696204
EvanCib <eciborowski@umass.edu>
2021-09-04 17:08:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+class Hw1Q1:
+        def polynomial(float):
+
+
+
+                a = float(input("a = : "))
+                b = float(input("b = : "))
+                c = float(input("c = : "))
+
+                x = float((c**2 / a) - (2 * b / a))
+
+                print("x = : ", x)
+
+                #print("You entered a: ", a)
+                #print("You entered b: ", b)
+                #print("You entered c: ", c)
+
+
+
+
+
+
+
+
+        def monomial(int):
+
+                print("Monomial ax-2b=c ")
+
+                a = float(input("a = : "))
+                b = float(input("b = : "))
+                c = float(input("c = : "))
+
+                x = float((c/a)+(2*b/a))
+
+                print ("x = : " ,x)
+
+
+                #print("You entered a: " , a)
+                #print("You entered b: " , b)
+                #print("You entered c: " , c)
+
+
+
+
+        monomial(int)
+        polynomial(float)
